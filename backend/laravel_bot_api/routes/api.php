c974a60bc2616e23ebb526c3cd139165bd84a48c
matheushenrique6 <matheushenriquecostaxavier6@gmail.com>
2025-09-12 10:57:28 -0300

    Remove unused routes, tests, and configuration files from the Laravel bot API project

--- backend/laravel_bot_api/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\MessageController;
-
-Route::get('/message', [MessageController::class, 'index']);
-Route::post('/messages', [MessageController::class, 'store']);
-Route::post('/send-message', [MessageController::class, 'sendToBot']);
